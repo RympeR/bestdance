de498b064d4986734abe2762120e7f50f03f452a
George <georg.rashkov@gmail.com>
2020-02-17 03:03:01 +0200

    auto_timing_v0.2

--- auto_timing.php
+++ auto_timing.php
@@ -6,10 +6,9 @@
 
 	//begining time in secs
 	$begining_time = $hours+$minutes;
-
+	$begining_time_2 = $hours+$minutes;
 	$amount_rest_day_1 = $_POST["amount_rest_day_1"];
 	$amount_rest_day_2 = $_POST["amount_rest_day_2"];
-
 	$rest_time = $_POST["duration_rest"];
 	
 	$minutes_rest = explode(':', $rest_time)[0]*60;
@@ -35,118 +34,85 @@
 	}
 
 	//day 1
-	$sql_28 = "SELECT id, COUNT(*) as row_amount, user_id,nomination, duration, team_name, dance_style, fio, city, file_name, TIME_TO_SEC(duration) as sec_dur  FROM `DATA_INPUT` WHERE nomination != 'Street Show' and nomination !='Show Girls' ORDER by min_age  , skill_level";
+	$sql_28 = "SELECT id,  user_id,nomination, duration, team_name, dance_style, fio, city, file_name, TIME_TO_SEC(duration) as sec_dur  FROM `DATA_INPUT` WHERE nomination != 'Street Show' and nomination !='Show Girls' ORDER by min_age  , skill_level";
 	//day 2
-	$sql_29 = 'SELECT id, COUNT(*) as row_amount, user_id,nomination, duration, team_name, dance_style, fio, city, file_name, TIME_TO_SEC(duration) as sec_dur FROM `DATA_INPUT` WHERE nomination = "Street Show" or  nomination ="Show Girls" ORDER by min_age  , skill_level;';
+	$sql_29 = 'SELECT id, user_id,nomination, duration, team_name, dance_style, fio, city, file_name, TIME_TO_SEC(duration) as sec_dur FROM `DATA_INPUT` WHERE nomination = "Street Show" or  nomination ="Show Girls" ORDER by min_age  , skill_level;' ;
 	
-	//max_form
-	$sql_max = 'SELECT max(id) as max_form from DATA_INPUT;';
+	$sql_amount_form_day_1 = "SELECT COUNT(*) as day1 FROM DATA_INPUT WHERE nomination != 'Street Show' and nomination !='Show Girls';" ;
 
+	$sql_amount_form_day_2 = "SELECT COUNT(*) as day2 FROM DATA_INPUT WHERE nomination = 'Street Show' or nomination ='Show Girls';";
 
-	$res_day_1 = $conn->query($sql_28);
-	$res_day_2 = $conn->query($sql_29);
-	$query_form_day_1 = $res_day_1->fetch_assoc();
-	$query_form_day_2 = $res_day_2->fetch_assoc();
-
-	$interval_day_1 = $query_form_day_1["row_amount"]/($amount_rest_day_1+ 1);
-	$interval_day_2 = $query_form_day_2["row_amount"]/($amount_rest_day_2 + 1);
-
+	$res_amount_for_day_1 = $conn->query($sql_amount_form_day_1);
+	$res_amount_for_day_2 = $conn->query($sql_amount_form_day_2);
 
 	$result_28 = $conn->query($sql_28);
 	$result_29 = $conn->query($sql_29);
-	$result_max = $conn->query($sql_max);
-	
-	//get result of sql_max query 
-	$amount_in_section = $result_max->fetch_assoc();
 
-	//how much dances in 1 section (before rest)
-	$interval = $amount_in_section["max_form"]/($amount_rest + 1);
-	
-	//day 1
-	
+	$day_1_count =  $res_amount_for_day_1->fetch_assoc();
+	$day_2_count =  $res_amount_for_day_2->fetch_assoc();
+
+	$interval_day_1 = round($day_1_count["day1"] / ($amount_rest_day_1 + 1));
+	$interval_day_2 = round($day_2_count["day2"] / ($amount_rest_day_1 + 1));
+	//max_form
+
+
+	// $interval_day_1 = $query_form_day_1["row_amount"]/($amount_rest_day_1+ 1);
+	// $interval_day_2 = $query_form_day_2["row_amount"]/($amount_rest_day_2 + 1);
+
 
 	
-	$counter = 1;
+	
+	$counter = 0;
 	//amount of rests
-	$counter_rest = 1;
-	//
-	$index = 1;
-	
+	$counter_rest_1 = $interval_day_1;
+	$counter_rest_2 = $interval_day_2;
 	$day1 = array();
 	$day2 = array();
 
-
-	while($day_1 = $result_28->fetch_assoc()) {
-		// if ($index%$amount_rest==0) {
-		// 	echo  $counter_rest . "::".$rest_time."::" .gmdate("H:i:s",$begining_time).":::"; 
-		// 	$begining_time+= $rest_time;
-		// 	$counter_rest++;
-		// 	echo $counter."::".$day_1["id"]."::".$day_1["duration"]."::".$day_1["nomination"]."::".$day_1["team_name"]."::".$day_1["dance_style"]."::".$day_1["fio"]."::".$day_1["city"]."::".$day_1["file_name"]."::".gmdate("H:i:s",$begining_time).":::";
-			
-		// 	$counter++;
-		// }
-		// else{
-			$row1 = $counter."::".$day_1["id"]."::".$day_1["duration"]."::".$day_1["nomination"]."::".$day_1["team_name"]."::".$day_1["dance_style"]."::".$day_1["fio"]."::".$day_1["city"]."::".$day_1["file_name"]."::".gmdate("H:i:s",$begining_time).":::";
-			$counter++;
-			array_push($day1, $row1);
-			$begining_time+=$day_1["sec_dur"]+60;
-		// }
-
-		// $index++;
-		// echo $result_info;
-	}
-	//day 2
-	
-	while($day_2 = $result_29->fetch_assoc()) {
-		// if ($index%$amount_rest==0) {
-		// 	echo $counter_rest . "::".$rest_time."::" .gmdate("H:i:s",$begining_time).":::"; 
-		// 	$begining_time+= $rest_time;
-		// 	$counter_rest++;
-		// 	$begining_time+=$day_2["sec_dur"];
-
-		// 	echo $counter."::".$day_2["id"]."::".$day_2["duration"]."::".
-		// 	$day_2["nomination"]."::".$day_2["team_name"]."::".$day_2["dance_style"]."::".$day_2["fio"]."::".$day_2["city"]."::".$day_2["file_name"]."::".gmdate("H:i:s",$begining_time).":::";
-		// 	$begining_time+=$day_2["sec_dur"]+60;
-		// 	$counter++;
-		// }else{
-
-			$row2 = $counter."::".$day_2["id"]."::".$day_2["duration"]."::".
-			$day_2["nomination"]."::".$day_2["team_name"]."::".$day_2["dance_style"]."::".$day_2["fio"]."::".$day_2["city"]."::".$day_2["file_name"]."::".gmdate("H:i:s",$begining_time).":::";
-			array_push($day2, $row2);
-			$begining_time+=$day_2["sec_dur"]+60;
-			// $counter++;
-		// }
-		// $index++;
-		// echo $result_info;
-	}
 	echo "28 Марта:::";
-	$chunked_day_1 = array_chunk($day1, $interval_day_1);
-	$chunked_day_2 = array_chunk($day2, $interval_day_2);
-	
-	var_dump($chunked_day_1);
-	var_dump($chunked_day_2);
+	while($day_1 = $result_28->fetch_assoc()) {
 
-	for ($i=0; $i < count($chunked_day_1); $i++) { 
-		for ($j=0; $j < count($chunked_day_1[i]) ; $j++) { 
-			echo $chunked_day_1[i][j];
+		if ($counter == $counter_rest_1) {
+			$counter++;
+			echo  $counter_rest . "::".$rest_time."::" .gmdate("H:i:s",$begining_time).":::"; 
+			$begining_time+= $rest_time;
+			$counter_rest++;
+			echo $counter."::".$day_1["id"]."::".$day_1["duration"]."::".$day_1["nomination"]."::".$day_1["team_name"]."::".$day_1["dance_style"]."::".$day_1["fio"]."::".$day_1["city"]."::".$day_1["file_name"]."::".gmdate("H:i:s",$begining_time).":::";
+			
+			$counter_rest_1 += $interval_day_1;
+		}
+		else{
+			$counter++;
+			echo $counter."::".$day_1["id"]."::".$day_1["duration"]."::".$day_1["nomination"]."::".$day_1["team_name"]."::".$day_1["dance_style"]."::".$day_1["fio"]."::".$day_1["city"]."::".$day_1["file_name"]."::".gmdate("H:i:s",$begining_time).":::";
 			$begining_time+=$day_1["sec_dur"]+60;
 		}
-		echo  $counter_rest . "::".$rest_time."::" .gmdate("H:i:s",$begining_time).":::"; 
-		$begining_time+= $rest_time;
+
 	}
+
+	$counter = 0;
+	//day 2
 	echo  "29 Марта:::";
 
-	for ($i=0; $i < count($chunked_day_2); $i++) { 
-		for ($j=0; $j < count($chunked_day_2[i]) ; $j++) { 
-			echo $chunked_day_2[i][j];
+	while($day_2 = $result_29->fetch_assoc()) {
+
+		if ($counter == $counter_rest_2) {
+			echo $counter_rest . "::".$rest_time."::" .gmdate("H:i:s",$begining_time_2).":::"; 
+			$begining_time_2+= $rest_time;
+			$counter_rest+=$interval_day_2;
+			$begining_time+=$day_2["sec_dur"];
+
+			$counter++;
+			echo $counter."::".$day_2["id"]."::".$day_2["duration"]."::".
+			$day_2["nomination"]."::".$day_2["team_name"]."::".$day_2["dance_style"]."::".$day_2["fio"]."::".$day_2["city"]."::".$day_2["file_name"]."::".gmdate("H:i:s",$begining_time).":::";
 			$begining_time+=$day_2["sec_dur"]+60;
+		}else{
+
+			$counter++;
+			echo $counter."::".$day_2["id"]."::".$day_2["duration"]."::".
+			$day_2["nomination"]."::".$day_2["team_name"]."::".$day_2["dance_style"]."::".$day_2["fio"]."::".$day_2["city"]."::".$day_2["file_name"]."::".gmdate("H:i:s",$begining_time).":::";
+			$begining_time+=$day_1["sec_dur"]+60;
 		}
-		echo  $counter_rest . "::".$rest_time."::" .gmdate("H:i:s",$begining_time).":::"; 
-		$begining_time+= $rest_time;
 	}
-
-
-
-	// echo $result_info;
+	
 
  ?>
